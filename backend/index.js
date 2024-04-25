2d4f7f4b1da94fb7d66014ba7b1bfdca277decfb
ME <adityakamble.quicklabs1@gail.com>
2024-04-25 22:22:36 +0530

    cors config

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: '',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 app.use(express.json());
 app.use("/users",router);
 app.use("/blog",blogRouter);
