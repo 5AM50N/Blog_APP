629252cac76a03035e3176746222fec262b22bda
ME <adityakamble.quicklabs1@gail.com>
2024-04-25 23:37:41 +0530

    reset

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-// app.use(cors());
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors());
+
 app.use(express.json());
 app.use("/users",router);
 app.use("/blog",blogRouter);
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_CONNECTION)
 .then(() => app.listen(port)).then(()=>console.log("listening at "+port)).catch((err)=>console.log(err));
 
